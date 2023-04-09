f041e203432d1aa5cfc60b92007bb3c0d6512c5b
Jasmine220 <dangdang220220@gmail.com>
2023-04-09 14:55:57 +0700

    done crud for staff

--- src/controllers/staffController.js
+++ src/controllers/staffController.js
@@ -1,72 +1,94 @@
 //declaring library
 const express = require("express");
-const Staff = require('../models/staff');
+const Staff = require("../models/staff");
 //using library
 const app = express();
 //set url, response
-app.use(express.json())
+app.use(express.json());
 
-//test login
-app.get("/login", (req, res) => {
-    res.send("LOGIN");
-  });
 //get all
-app.get('/staffs', async(req, res) => {
-    try {
-      const staffs = await Staff.find({});
-      res.status(200).json(staffs);
-  
-    } catch (error) {
-      res.status(500).json({message: error.message})
+exports.getAllStaffs = async (req, res) => {
+  try {
+    const staffs = await Staff.find({});
+    res.status(200).json(staffs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+//get by id
+exports.getStaffById = async (req, res) => {
+  try {
+  // var staffId = req.params.staffId;
+  // Staff.findOne({ _id: staffId }).then(
+  //   (data) => {
+  //     res.send(data);
+  //   },
+  //   (e) => {
+  //     res.status(400).send(e);
+  //   }
+  // );
+  const id = { _id: req.params.staffId };
+  const staff = await Staff.findById(id);
+  //check if staff exist
+    if(!staff){
+      return res.status(404).json({message:`Cannot find any staff in list`})
     }
-  })
-  //
-  //get by id
-  app.get('/staffs/:id', async(req, res) =>{
-    try {
-      const {id} = req.params;
-      const staff = await Staff.findById(id);
-      res.status(200).json(staff);
-    } catch (error) {
-      res.status(500).json({message:error.message})
+    res.status(200).json(staff);
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+};
+//update a staff
+exports.updateStaff = async (req, res) => {
+  const id = { _id: req.params.staffId };
+  Staff.findByIdAndUpdate(id, req.body)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((e) => {
+      res.status(400).send(`Cannot find any staff in list`);
+    });
+  // try {
+  //   // const {id} = req.params;
+  //   const query = { _id: req.params.staffId };
+  //   const staff = await Staff.findByIdAndUpdate(query,req.body);
+  //   //check if staff exist
+  //   if(!staff){
+  //     return res.status(404).json({message:`Cannot find any staff in list`})
+  //   }
+  //   return res.status(200).json(staff)
+  // } catch (error) {
+  //   res.status(500).json({message:error.message})
+  // }
+};
+//create a staff
+exports.createStaff = async (req, res) => {
+  try {
+    const staff = await Staff.create(req.body);
+    res.status(200).json(staff);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+//delete a staff
+exports.deleteStaff = async (req, res) => {
+  // const id = { _id: req.params.staffId };
+  // Staff.findByIdAndDelete(id, req.body)
+  //   .then((data) => {
+  //     res.send(data);
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send(`Cannot find any staff in list`);
+  //   });
+  try {
+    const id = { _id: req.params.staffId };
+    const staff = await Staff.findByIdAndDelete(id);
+    if(!staff){
+      return res.status(404).send(`Cannot find any staff in list`);
     }
-  })
-  //update a staff
-  app.put('/staffs/:id', async(req, res)=>{
-    try {
-      const {id} = req.params;
-      const staff = await Staff.findByIdAndUpdate(id,req.body);
-      //check if staff exist
-      if(!staff){
-        return res.status(404).json({message:`Cannot find any staff in list`})
-      }
-      return res.status(200).json(staff)
-    } catch (error) {
-      res.status(500).json({message:error.message})
-    }
-  })
-  //create a staff
-  app.post('/staff', async (req, res) => {
-      try {
-          const staff = await Staff.create(req.body)
-          res.status(200).json(staff);
-  
-      } catch (error) {
-          console.log(error.message);
-          res.status(500).json({message: error.message})
-      }
-  })
-  //delete a staff
-  app.delete('/staffs/:id', async(req, res) =>{
-    try {
-      const {id} = req.params;
-      const staff = await Staff.findByIdAndDelete(id);
-      if(!staff){
-        return res.status(404).json({message:`can't find any staff with ID ${id}`})
-      }
-      res.status(200).json(staff);
-    } catch (error) {
-      res.status(500).json({message: error.message})
-    }
-  })
-
+    res.status(200).json(staff);
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+};
--- src/routes/staffRoute.js
+++ src/routes/staffRoute.js
@@ -1,14 +1,14 @@
-// const staffBuilder = require('../controllers/staffController');
+const staffBuilder = require('../controllers/staffController');
 
-// module.exports = app => {
-//   app
-//     .route('/staffs')
-//     .get(staffBuilder.getAllStaffs)
-//     .post(staffBuilder.create);
+module.exports = app => {
+  app
+    .route('/staffs')
+    .get(staffBuilder.getAllStaffs)
+    .post(staffBuilder.createStaff);
 
-//   app
-//     .route('/staffs/:staffId')
-//     .get(staffBuilder.getById)
-//     .put(staffBuilder.update)
-//     .delete(staffBuilder.delete);
-// };
+  app
+    .route('/staffs/:staffId')
+    .get(staffBuilder.getStaffById)
+    .put(staffBuilder.updateStaff)
+    .delete(staffBuilder.deleteStaff);
+};
